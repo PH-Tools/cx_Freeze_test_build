020a831a0fe1a4166bd102321f504253b55dec59
PH Tools <phtools@bldgtyp.com>
2023-11-10 16:12:12 +0100

    test

--- CarbonCheck.py
+++ CarbonCheck.py
@@ -1,5 +1,13 @@
 from PyQt6.QtWidgets import QApplication, QLabel
+
+import honeybee
+import ladybug
+import ladybug_rhino
+import honeybee_energy
+import honeybee_ph
+import PHX
 import ph_units
+import ph_baseliner
 
 app = QApplication([])
 label = QLabel(f"This is a test: {ph_units}")
